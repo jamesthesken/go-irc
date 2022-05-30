893a8de7b3a9ac6d453bb16fc2051ae638754ea8
jamesthesken <jamesthesken@gmail.com>
2022-05-30 13:25:53 -1000

    Refactor ping pong messages

--- client.go
+++ client.go
@@ -24,39 +24,8 @@ type Client struct {
 	nick    string
 }
 
-var commands = map[string]func(client *Client, userInput string){
-	"/nick": func(client *Client, userInput string) { nick(client, userInput) },
-}
-
-func nick(client *Client, userInput string) {
-	msg := strings.Split(userInput, " ")
-	client.nick = msg[1]
-}
-
-// TODO: implement multi-line messages
-func (cli *CLI) Write(client io.Writer) {
-	writer := bufio.NewWriter(client)
-	reader := bufio.NewReader(cli.in)
-
-	for {
-		str, err := reader.ReadString('\n')
-		if err != nil {
-			log.Print(str)
-			log.Fatalf("Error reading input: %s", err)
-		}
-
-		// Just makes for easier formatting, as opposed to WriteString()
-		fmt.Fprintf(writer, "%s\r\n", str)
-		writer.Flush()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-	}
-}
-
 // Server operations
-func Connect(server string) (net.Conn, error) {
+func (client *Client) Connect(server string) (net.Conn, error) {
 	c, err := net.Dial("tcp", server)
 	if err != nil {
 		log.Fatalf("Error: %s", err)
@@ -67,14 +36,40 @@ func Connect(server string) (net.Conn, error) {
 	nick := "NICK samej1293871\n"
 	user := "USER samej1293871 * * :samej1293871\n"
 
-	c.Write([]byte(nick))
+	client.nick = nick
 
+	c.Write([]byte(nick))
 	c.Write([]byte(user))
 
 	return c, nil
 }
 
-var wg sync.WaitGroup
+// TODO: implement multi-line messages
+func (cli *CLI) Write(conn io.Writer) {
+	writer := bufio.NewWriter(conn)
+	reader := bufio.NewReader(cli.in)
+	client := &Client{}
+
+	for {
+		str, err := reader.ReadString('\n')
+		if err != nil {
+			log.Print(str)
+			log.Fatalf("Error reading input: %s", err)
+		}
+
+		msg := client.formatMessage(str)
+		log.Print(msg)
+		log.Print(client)
+
+		// Just makes for easier formatting, as opposed to WriteString()
+		fmt.Fprintf(writer, "%s\r\n", msg)
+		writer.Flush()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
 
 // This may be better implemented as two separate functions
 // One to read incoming messages
@@ -84,16 +79,8 @@ func (cli *CLI) Read(client io.ReadWriter) {
 	s := bufio.NewScanner(client)
 	for s.Scan() {
 		line := s.Text()
-
 		fmt.Printf("%s\n", parseMessage(line))
 
-		if strings.Contains(line, "PING") {
-			msg := strings.TrimPrefix(line, "PING")
-
-			pong := "PONG" + msg
-			client.Write([]byte(pong))
-			log.Printf("Client sent: %s", pong)
-		}
 	}
 	if s.Err() != nil {
 		log.Fatalf("Error occured: %s", s.Err())
@@ -104,24 +91,31 @@ func (cli *CLI) Read(client io.ReadWriter) {
 
 // parseMessage returns formatted incoming messages
 func parseMessage(msg string) string {
-	timeStamp := time.Now()
 	// kinda wonky, this assumes incoming messages are always in the leading format:
 	// ":Guest56!~Guest56@cpe-10.21.123.13.1.foo.bar.com PRIVMSG #python"
 	// with that assumption, after the split we only need
 	// everything after index 3:
 	fullMsg := strings.Split(msg, " ")
 
-	content := strings.Join(fullMsg[3:][:], " ")
+	// check for ping messages
+	if fullMsg[0] == "PING" {
+		pong := "PONG" + fullMsg[1]
+		return pong
+	} else {
+		content := strings.Join(fullMsg[3:][:], " ")
 
-	// nickname of who we received the message from
-	msgNick := strings.Split(fullMsg[0], "!")[0]
+		// nickname of who we received the message from
+		msgNick := strings.Split(fullMsg[0], "!")[0]
 
-	formatted := fmt.Sprintf("%s < %s > %s",
-		timeStamp.Format("3:04PM"),
-		strings.TrimPrefix(msgNick, ":"),
-		strings.TrimPrefix(content, ":"))
+		timeStamp := time.Now()
 
-	return formatted
+		formatted := fmt.Sprintf("%s < %s > %s",
+			timeStamp.Format("3:04PM"),
+			strings.TrimPrefix(msgNick, ":"),
+			strings.TrimPrefix(content, ":"))
+		return formatted
+	}
+
 }
 
 // formatMessage returns formatted outgoing messages
@@ -132,32 +126,46 @@ func (client *Client) formatMessage(msg string) string {
 		// Maybe a bad assumption that we can properly split the string
 		// e.g. - '/nick james' will become [/nick, james]
 		cmd := strings.Split(msg, " ")[0]
-
-		// If the string value of the command exists in the commands map,
-		// execute the state-changing function -> see commands.go for details
-		if val, exists := commands[cmd]; exists {
-			val(client, msg)
-		} else {
-			return cmd
+		// see commands.go for details
+		err := executeCommand(client, msg, cmd)
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		return strings.TrimPrefix(msg, "/")
 	}
 
-	return "formatted"
+	// otherwise the message is sent to the current channel
+	// needs to change to handle private messages of course!
+	//  ":james!~james@cpe-10.21.123.13.1.foo.bar.com PRIVMSG #go-nuts :Hello!"
+	outMsg := fmt.Sprintf("PRIVMSG %s :%s",
+		strings.TrimSuffix(client.channel, "\n"),
+		msg)
+
+	return outMsg
 
 }
+
+var wg sync.WaitGroup
 
 func main() {
 	wg.Add(1)
 
 	cli := CLI{os.Stdin}
 
-	client, err := Connect("irc.libera.chat:6667")
+	client := &Client{}
+
+	name, err := os.Hostname()
+
+	client.host = name
+
+	conn, err := client.Connect("irc.libera.chat:6667")
 	if err != nil {
 		log.Fatalf("Error: %s", err)
 	}
 
-	go cli.Read(client)
-	go cli.Write(client)
+	go cli.Read(conn)
+	go cli.Write(conn)
 	wg.Wait()
 
 }
