64337bb6290eac0f3d81229ca9648d31a0fe23a0
jamesthesken <jamesthesken@gmail.com>
2022-05-30 14:20:25 -1000

    remove client connection tests

--- irc_test.go
+++ irc_test.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"log"
 	"net"
 	"testing"
 	"time"
@@ -36,34 +33,6 @@ func TestServer(t *testing.T) {
 }
 
 func TestClient(t *testing.T) {
-	// t.Run("test if the client write to the server", func(t *testing.T) {
-	// 	client, err := net.Dial(network, address)
-	// 	assertNoError(t, err)
-	// 	defer client.Close()
-
-	// 	// Set timeout so we can test cli.Read()
-	// 	client.SetDeadline(time.Now().Add(time.Second))
-
-	// 	// Simulate user input
-	// 	in := strings.NewReader("Hello, world!\n")
-	// 	cli := &CLI{in}
-
-	// 	var wg sync.WaitGroup
-	// 	wg.Add(1)
-
-	// 	go func() {
-	// 		cli.Write(client)
-	// 	}()
-
-	// 	got := cli.testRead(client)
-	// 	want := "Hello, world!"
-
-	// 	if got != want {
-	// 		t.Errorf("got %q, wanted %q", got, want)
-	// 	}
-
-	// })
-
 	t.Run("test ingress message formatting", func(t *testing.T) {
 		got := parseMessage(":Guest56!~Guest56@cpe-10.21.123.13.1.foo.bar.com PRIVMSG #python :Please use a test channel https://foo.bar.com")
 
@@ -113,17 +82,4 @@ func assertNoError(t testing.TB, got error) {
 	if got != nil {
 		t.Fatalf("Received an error, %s", got)
 	}
-}
-
-// Test client read by returning a string
-func (cli *CLI) testRead(client io.Reader) string {
-	s := bufio.NewScanner(client)
-	for s.Scan() {
-		line := s.Text()
-		return line
-	}
-	if s.Err() != nil {
-		log.Fatalf("Error occured: %s", s.Err())
-	}
-	return ""
 }
